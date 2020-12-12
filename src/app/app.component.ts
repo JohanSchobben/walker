ea9ddc6f93ae11d816a00332e9061a6fcb04cfad
Johan Schobben <johanschobben@gmail.com>
2020-12-12 19:10:45 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wlk-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'walker';
+}
